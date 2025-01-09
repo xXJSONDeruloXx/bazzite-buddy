f87044cb68b60e008b777c2359eb671cb09eb6a1
xXJSONDeruloXx <danielhimebauch@gmail.com>
2025-01-09 17:41:38 -0500

    chore: remove unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,5 @@
-import {
-  ButtonItem,
-  PanelSection,
-  PanelSectionRow,
-  Navigation,
-  staticClasses
-} from "@decky/ui";
-import {
-  addEventListener,
-  removeEventListener,
-  callable,
-  definePlugin,
-  toaster,
-  // routerHook
-} from "@decky/api"
-import { useState } from "react";
-import { FaShip } from "react-icons/fa";
-
-// import logo from "../assets/logo.png";
-
-// This function calls the python function "add", which takes in two numbers and returns their sum (as a number)
-// Note the type annotations:
-//  the first one: [first: number, second: number] is for the arguments
-//  the second one: number is for the return value
-const add = callable<[first: number, second: number], number>("add");
-
-// This function calls the python function "start_timer", which takes in no arguments and returns nothing.
-// It starts a (python) timer which eventually emits the event 'timer_event'
-const startTimer = callable<[], void>("start_timer");
+import { definePlugin } from "decky-frontend-lib";
+import React from "react";
 
 function Content() {
   return (
@@ -45,15 +18,9 @@ function Content() {
 }
 
 export default definePlugin(() => {
-  console.log("WebPage Viewer plugin initializing");
-
   return {
     name: "WebPage Viewer",
-    titleView: <div className={staticClasses.Title}>WebPage Viewer</div>,
     content: <Content />,
-    icon: <FaShip />,
-    onDismount() {
-      console.log("WebPage Viewer plugin unloaded");
-    },
+    onDismount() {},
   };
 });
